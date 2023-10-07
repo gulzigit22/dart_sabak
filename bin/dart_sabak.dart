96f9f3dfdfb68ecddd8f10a865c1543aa48cc2aa
gulzigit22 <gulzigittalant@gmail.com>
2023-10-07 18:30:45 +0600

    dart sabak

---
+++ bin/dart_sabak.dart
@@ -0,0 +1,57 @@
+void main() {
+  List a = [12, 'ddd', true, 33];
+  a.
+      // List b = [12, 333, 22, 33];
+      // a.addAll(b);
+      // // print(a[0]);
+      // print(a);
+      // // // a.add(21);
+      // // a.clear();
+      // // print(a.length);
+      // print(a.contains('ddd'));
+
+      // // List<dynamic> list = List.filled(5, 0);
+      // // list[0] = 4;
+      // // list[1] = 'dfg';
+      // // list[2] = true;
+
+      // // print(list);
+      Map map = {22: 552, 'dfgh': 55, true: 'sdfg'};
+  Map map2 = {2: 55, 'dfg': 50, false: 'ikm,'};
+
+  map.addAll(map2);
+  map.clear();
+
+  print(map.length);
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+  // String almaBak = '22'; // camel case:
+  // String alma_bak_12 = "a'sdfgd   "; //snak case:
+  // String alma_bak_ = '''ASDFGH
+  // SDFGHJK'''; //sn
+  // int c = 23;
+  // int a = 22;
+  // print(c % a);
+
+  // print(almaBak);
+  // print(alma_bak_12 + '  ' + alma_bak_);
+  // print(alma_bak_);
+
+  // int a = 13;
+  // int b = 40;
+
+  // bool ss = a >= b;
+  // print(ss);
+  ////////////////////////////////////////////////////////////////////
