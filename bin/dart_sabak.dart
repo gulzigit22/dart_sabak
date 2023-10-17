4315abcb667888e3af4e781feb3faaf6bc729681
gulzigit22 <gulzigittalant@gmail.com>
2023-10-17 23:09:31 +0600

    dart sabak for sikil

--- bin/dart_sabak.dart
+++ bin/dart_sabak.dart
@@ -1,57 +1,90 @@
 void main() {
-  List a = [12, 'ddd', true, 33];
-  a.
-      // List b = [12, 333, 22, 33];
-      // a.addAll(b);
-      // // print(a[0]);
-      // print(a);
-      // // // a.add(21);
-      // // a.clear();
-      // // print(a.length);
-      // print(a.contains('ddd'));
+  // List<String> a = ['alma', 'banan', 'juzum'];
 
-      // // List<dynamic> list = List.filled(5, 0);
-      // // list[0] = 4;
-      // // list[1] = 'dfg';
-      // // list[2] = true;
+  // int b = a.length;
 
-      // // print(list);
-      Map map = {22: 552, 'dfgh': 55, true: 'sdfg'};
-  Map map2 = {2: 55, 'dfg': 50, false: 'ikm,'};
+  // for (int i = 0; i < b; i++) {
+  //   print(b);
+  // }
 
-  map.addAll(map2);
-  map.clear();
+  // List a = [12, 'ddd', true, 33];
 
-  print(map.length);
+  // List b = [12, 333, 22, 33];
+  // a.addAll(b);
+  // // print(a[0]);
+  // print(a);
+  // // // a.add(21);
+  // // a.clear();
+  // // print(a.length);
+  // print(a.contains('ddd'));
+
+  // // List<dynamic> list = List.filled(5, 0);
+  // // list[0] = 4;
+  // // list[1] = 'dfg';
+  // // list[2] = true;
+
+  // // print(list);
+  // Map map = {22: 552, 'dfgh': 55, true: 'sdfg'};
+  // Map map2 = {2: 55, 'dfg': 50, false: 'ikm,'};
+
+  // map.addAll(map2);
+  // map.clear();
+
+  // print(map.length);
+  ///////////////////////////////////////////////////
+  //  == равно
+  // != не равно
+  // <= Меньше или равно
+  // >= Больше или равно
+  // >  Больше
+  // < Меньше
+
+// && (и)
+// || (или)
+
+  // int balans = 101;
+  // int avtobuws = 40;
+  // int taxsi = 100;
+  // int biznes = 500;
+
+  // if (balans >= avtobuws && balans <= taxsi) {
+  //   print('avtobus menen ketem');
+  // } else if (balans >= taxsi && balans <= biznes) {
+  //   print('taxsi menen ketem');
+  // } else if (balans >= biznes) {
+  //   print('biznes menen ketem');
+  // } else {
+  //   print('peshkom ketem');
+  // }
+
+  // int balans = 0;
+  // int avtobuws = 40;
+  // int taxsi = 100;
+  // int biznes = 500;
+
+  // if (balans >= avtobuws && balans <= taxsi) {
+  //   return print('avtobus menen ketem');
+  // } else if (balans >= taxsi && balans < biznes) {
+  //   return print('taxsi menen ketem');
+  // }
+  //////////////////////////////////////////////////
 }
 
+// String almaBak = '22'; // camel case:
+// String alma_bak_12 = "a'sdfgd   "; //snak case:
+// String alma_bak_ = '''ASDFGH
+// SDFGHJK'''; //sn
+// int c = 23;
+// int a = 22;
+// print(c % a);
 
+// print(almaBak);
+// print(alma_bak_12 + '  ' + alma_bak_);
+// print(alma_bak_);
 
+// int a = 13;
+// int b = 40;
 
-
-
-
-
-
-
-
-
-
-  // String almaBak = '22'; // camel case:
-  // String alma_bak_12 = "a'sdfgd   "; //snak case:
-  // String alma_bak_ = '''ASDFGH
-  // SDFGHJK'''; //sn
-  // int c = 23;
-  // int a = 22;
-  // print(c % a);
-
-  // print(almaBak);
-  // print(alma_bak_12 + '  ' + alma_bak_);
-  // print(alma_bak_);
-
-  // int a = 13;
-  // int b = 40;
-
-  // bool ss = a >= b;
-  // print(ss);
-  ////////////////////////////////////////////////////////////////////
+// bool ss = a >= b;
+// print(ss);
+////////////////////////////////////////////////////////////////////
